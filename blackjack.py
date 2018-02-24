01e48efeb18ac1d57035b5fb423387bdd0838da1
BSH <bruce@redefinedprop.com>
2018-02-24 21:33:07 +0000

    build more test

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,11 @@
 import os
 import random
 
+cards = ["2","3","4","5","6","7","8","9","10", "J", "Q", "K", "A"]
+picture_cards = ["J", "Q", "K"]
+ace = "A"
+hand = []
+
 # creates a card shoe.  A card shoe is the device that holds multiple of decks of cards.
 
 def card_shoe(num = 1):
@@ -16,9 +21,22 @@ def card_shoe(num = 1):
     if num < 1:
         num = 1
         
-    deck = ["2","3","4","5","6","7","8","9","10", "J", "Q", "K", "A"] * 4
+    deck = cards * 4
     shoe = deck * num
     return shoe
+    
+def card_val(card):
+    if card in picture_cards:
+        return 10
+    elif card == ace:
+        return (1, 11)
+    else:
+        return int(card)
+
+def hand_val(dealthand = []):
+    return hand_val()
+
+
     
 
 
--- test_bj.py
+++ test_bj.py
@@ -28,6 +28,25 @@ class  TestBlackjack(unittest.TestCase):
         self.assertEqual(blackjack.card_shoe(-1), test_deck*1)
         self.assertEqual(blackjack.card_shoe(4.5), test_deck*1)
         self.assertEqual(blackjack.card_shoe("a"), test_deck*1)
+    
+    """
+    Test if the value of card is correct
+
+    """
+    
+    def test_cardvalue(self):
+        self.assertEqual(blackjack.card_val("J"), 10)
+        self.assertEqual(blackjack.card_val("Q"), 10)
+        self.assertEqual(blackjack.card_val("K"), 10)
+        self.assertEqual(blackjack.card_val("2"), 2)
+        self.assertEqual(blackjack.card_val("5"), 5)
+        self.assertEqual(blackjack.card_val("9"), 9)
+        self.assertEqual(blackjack.card_val("A"), (1, 11))
+        
+    def test_handvalue(self):
+        self.assertEqual(blackjack.hand_val(["1","2","9"]), 12)
+        
+        
         
 
 if __name__ == '__main__':
